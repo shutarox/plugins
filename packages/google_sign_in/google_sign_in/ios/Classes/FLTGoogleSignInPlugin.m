290145bcce3f2b6fcefa1e5767f45381e04fda3d
Shuhei Kawasaki <kawasaki@dena.jp>
2020-09-28 06:18:47 +0900

    test

--- packages/google_sign_in/google_sign_in/ios/Classes/FLTGoogleSignInPlugin.m
+++ packages/google_sign_in/google_sign_in/ios/Classes/FLTGoogleSignInPlugin.m
@@ -112,8 +112,7 @@ static FlutterError *getFlutterError(NSError *error) {
       result(error != nil ? getFlutterError(error) : @{
         @"idToken" : authentication.idToken,
         @"accessToken" : authentication.accessToken,
-        @"serverAuthCode" : authentication.accessToken,
-//        @"serverAuthCode" : currentUser.serverAuthCode,
+        @"serverAuthCode" : currentUser.serverAuthCode != nil ? currentUser.serverAuthCode : @"EMPTY",
       });
     }];
   } else if ([call.method isEqualToString:@"signOut"]) {
